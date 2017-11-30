c0923af78f79dd4c291a05f11655df9c1c707d3b
Nick Kapetanas <nkapetanas@hotmail.com>
2017-11-30 23:57:30 +0200

    added test, train and main

--- neural_network.py
+++ neural_network.py
@@ -32,7 +32,7 @@ def load_data():
     # load train data in N*D array (60000x784 for MNIST)
     #                              divided by 255 to achieve normalization
     train_data = np.array([[j for j in i.split(" ")] for i in tmp], dtype='int') / 255
-    print "Train data array size: ", train_data.shape
+    print ("Train data array size: ", train_data.shape)
     tmp = []
     for i in test_files:
         with open(i, 'r') as fp:
@@ -40,7 +40,7 @@ def load_data():
     # load test data in N*D array (10000x784 for MNIST)
     #                             divided by 255 to achieve normalization
     test_data = np.array([[j for j in i.split(" ")] for i in tmp], dtype='int') / 255
-    print "Test data array size: ", test_data.shape
+    print ("Test data array size: ", test_data.shape)
     tmp = []
     for i, _file in enumerate(train_files):
         with open(_file, 'r') as fp:
@@ -126,33 +126,147 @@ class ML_NeuralNetwork:
         self.weights2 = np.random.rand(self.number_of_outputs, self.hidden_neurons + 1)
 
 
-        def feedForward(self, x, t, weights1, weights2):
+    def feedForward(self, x, t, weights1, weights2):
 
-            # We calculate first the dot product between weights1 and x and then we pass it as an arg in the chosen act_fun and its gradient func
-            firstLayerResult = self.activation_function(np.dot(x,weights1.T))
+        # We calculate first the dot product between weights1 and x and then we pass it as an arg in the chosen act_fun and its gradient func
+        firstLayerResult = self.activation_function(np.dot(x,weights1.T))
 
-            # Add bias
-            firstLayerResult = np.hstack((np.ones((firstLayerResult.shape[0], 1)), firstLayerResult))
+        # Add bias
+        firstLayerResult = np.hstack((np.ones((firstLayerResult.shape[0], 1)), firstLayerResult))
 
-            # Y is the output
-            y = np.dot(firstLayerResult, weights2.T)
-            # softmaxResult is the probability
-            softmaxResult = softmax(y)
-            max_error = np.max(softmaxResult, axis=1)
+        # Y is the output
+        y = np.dot(firstLayerResult, weights2.T)
+        # softmaxResult is the probability
+        softmaxResult = softmax(y)
+        max_error = np.max(softmaxResult, axis=1)
 
-            # Loss function
-            Ew =  np.sum(t * y) - np.sum(max_error) - \
-                np.sum(np.log(np.sum(np.exp(y - np.array([max_error, ] * y.shape[1]).T), 1))) - \
-                (0.5 * lamda) * np.sum(np.square(weights2))
+        # Loss function
+        Ew =  np.sum(t * y) - np.sum(max_error) - \
+            np.sum(np.log(np.sum(np.exp(y - np.array([max_error, ] * y.shape[1]).T), 1))) - \
+            (0.5 * lamda) * np.sum(np.square(weights2))
 
-            # Gradient ascent calculation for W2, (T-Y).T*Z -λW2
+        # Gradient ascent calculation for W2, (T-Y).T*Z -λW2
 
-            grand2 = np.dot((t-y).T , firstLayerResult) -eta* weights2
+        grand2 = np.dot((t-y).T , firstLayerResult) -eta* weights2
 
-            # Gradient ascent calculation for W1
-            # TODO
+        # Gradient ascent calculation for W1
+        # TODO
 
-            return Ew, grand2
+        return Ew, grand2
+
+    def neural_network_train(self):
+        Ew_old = -np.inf
+        for i in range(self.number_of_iteration):
+            error, gradWeight1, gradWeight2 = feedForward(self.X_train,self.t,self.weights1,self.weights2)
+            print("iteration #", i, "and error=", error)
+
+            if np.absolute(error- Ew_old) < self.tolerance:
+                    break
+            # grapse ton tipo
+            self.weights1 += self.eta * gradWeight1
+
+            self.weights2 += self.eta * gradWeight2
+            Ew_old = error
+
+
+    def neural_network_test(self,test_data,test_truth_data):
+        # First we add in the test data the bias
+        self.test_data = np.hstack((np.ones((test_data.shape[0], 1)), test_data))
+
+        #Feed forward
+
+        resultsOfActF = self.activation_function(np.dot(test_data,self.weights1.T))
+
+        # We now the bias
+        resultsOfActF = np.hstack((np.ones((resultsOfActF.shape[0], 1)), resultsOfActF))
+
+        y = np.dot(resultsOfActF, self.weights2.T)
+
+        probabilitiesResult = softmax(y)
+
+        decision = np.max(probabilitiesResult, axis=1)
+
+        error = 0
+
+        for i in range(len(test_truth_data)):
+            if np.argmax(test_truth_data[i]) != decision[i]:
+                error +=1
+
+        print("The Error is", error/test_truth_data.shape[0] * 100, "%")
+
+
+    def grad_check(self):
+        epsilon = 1e-6
+        _list = np.random.randint(self.X_train.shape[0], size=5)
+        x_sample = np.array(self.x[_list, :])
+        t_sample = np.array(self.t[_list, :])
+
+        Ew, gradWeight1, gradWeight2 = self.feedForward(x_sample,t_sample,self.weights1,self.weights2)
+
+        print("gradWeight1 shape: ", gradWeight1.shape)
+        print("gradWeight2 shape: ", gradWeight2.shape)
+        numericalGrad1 = np.zeros(gradWeight1.shape)
+        numericalGrad2 = np.zeros(gradWeight2.shape)
+
+        #W1 gradcheck
+        for k in range(0,numericalGrad1.shape[0]):
+            for d in range(0,numericalGrad1.shape[1]):
+                w_temp = np.copy(self.weights1)
+                w_temp[k,d] += epsilon
+                e_plus, _, _ = self.feedForward(x_sample,t_sample,w_temp,self.weights2)
+
+                w_tmp = np.copy(self.weights1)
+                w_tmp[k, d] -= epsilon
+                e_minus, _, _ = self.feedForward(x_sample, t_sample, w_tmp, self.weights2)
+                numericalGrad1[k, d] = (e_plus - e_minus) / (2 * epsilon)
+
+        # Absolute norm
+
+        print("The difference estimate for gradient of w1 is : ",
+                np.amax(np.abs(gradWeight1 - numericalGrad1)))
+
+        # W2 gradcheck
+        for k in range(0, numericalGrad2.shape[0]):
+            for d in range(0, numericalGrad2.shape[1]):
+                w_temp = np.copy(self.weights2)
+                w_temp[k, d] += epsilon
+                e_plus, _, _ = self.feedForward(x_sample, t_sample, w_temp, self.weights1)
+
+                w_tmp = np.copy(self.weights1)
+                w_tmp[k, d] -= epsilon
+                # isos thelei na pername prota to weights kai meta to tmp gia tin w2
+                e_minus, _, _ = self.feedForward(x_sample, t_sample, w_tmp, self.weights1)
+                numericalGrad1[k, d] = (e_plus - e_minus) / (2 * epsilon)
+
+        # Absolute norm
+
+        print("The difference estimate for gradient of w2 is : ",
+                np.amax(np.abs(gradWeight2 - numericalGrad2)))
+
+
+
+if __name__ == '__main__':
+   print("Neural Network multi classification, for Mnist dataset")
+
+   act_func = int(input("Please choose an activation function( insert a number between 1-3): 1 for logarithmic, 2 for tanh and 3 for cosine "))
+   hidden_units = int(input("Please choose the number of hidden neurons:"))
+
+   act_func=int(act_func)
+   hidden_units = int(hidden_units)
+   x_data, test_data, train_truth_data, test_truth_data = load_data()
+   lamda = 0.1
+   eta = 0.5 / x_data.shape[0]
+   # Maximum number of iteration of gradient ascend
+   number_of_iterations = 500
+   tolerance = 1e-6
+   mlnn = ML_NeuralNetwork(x_data, act_func, hidden_units, lamda, number_of_iterations, train_truth_data, eta, tolerance)
+
+   mlnn.grad_check()
+   mlnn.train()
+   mlnn.test(test_data,test_truth_data)
+
+
+
 
 
         # Documentation
